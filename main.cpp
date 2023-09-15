16ed993a60171084199f6c9eca73e1128a8f24c0
Florian Rakos <58404248+FlorianRakos@users.noreply.github.com>
2023-09-15 21:07:20 +0200

    Process JSON, Quit on key press

--- main.cpp
+++ main.cpp
@@ -1,13 +1,80 @@
 #include <iostream>
 #include <Windows.h>
 #include <chrono>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <string>
+#include <stdlib.h>
+
+//mousetrap
 
 LRESULT CALLBACK EventHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int deltaX = 0;
 int deltaY = 0;
+rapidjson::Document document;
+rapidjson::Value dataArray(rapidjson::kArrayType);
+
+
+void saveJSON () {
+    std::cout << "-------Save JSON-------" << std::endl;
+    // Add the dataArray to the document
+    document.AddMember("data", dataArray, document.GetAllocator());
+
+    // Serialize the document to a JSON string
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    document.Accept(writer);
+
+    // Print the JSON string
+    std::string jsonString = buffer.GetString();
+    std::cout << "JSON: " << jsonString << std::endl;
+}
+
+void processJSON (double time, int deltaX, int deltaY) {
+
+    // Create a data object
+    rapidjson::Value dataObject(rapidjson::kObjectType);
+
+    // Add timestamp, deltaX, and deltaY to the data object
+    dataObject.AddMember("timestamp", time, document.GetAllocator());
+    dataObject.AddMember("deltaX", deltaX, document.GetAllocator());
+    dataObject.AddMember("deltaY", deltaY, document.GetAllocator());
+
+    // Add the data object to the dataArray
+    dataArray.PushBack(dataObject, document.GetAllocator());
+
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    dataArray.Accept(writer);
+
+    // Print the JSON string
+    std::string jsonString = buffer.GetString();
+    //std::cout << "JSON: " << jsonString << std::endl;
+
+ // print json
+    //saveJSON(dataArray);
+
+}
+
+bool isKeyPressed(int keyCode) {
+    return GetAsyncKeyState(keyCode) & 0x8000;
+}
 
 int main() {
+
+    std::atexit(saveJSON);
+
+    //Prepare json
+    document.SetObject();
+
+    // Add the sessionID as a string
+    rapidjson::Value sessionID;
+    sessionID.SetString("1", document.GetAllocator());
+    document.AddMember("sessionID", sessionID, document.GetAllocator());
+
+
 
     HINSTANCE hInstance = GetModuleHandle(NULL);
     const char *class_name = "SimpleEngine Class";
@@ -44,7 +111,8 @@ int main() {
 
 
     auto counter = std::chrono::system_clock::now();
-    double timeDelay = 0.001; //s
+    double timeDelay = 0.005; //s
+
 
     while (!quit) {
         while (PeekMessage(&event, 0, 0, 0, PM_REMOVE)) {
@@ -57,16 +125,28 @@ int main() {
 
         }
         auto now = std::chrono::system_clock::now();
+        auto durationSinceEpoch = now.time_since_epoch();
+        long long msSinceEpoch = std::chrono::duration_cast<std::chrono::milliseconds>(durationSinceEpoch).count();
+
         std::chrono::duration<double> elapsed = now - counter;
         double timeDiff = elapsed.count();
 
 
-            if (timeDiff >= timeDelay) {
-                std::cout << "Time: " << timeDiff << " DeltaX: " << deltaX << " DeltaY: " << deltaY << std::endl;
-                deltaX = 0;
-                deltaY = 0;
-                counter = std::chrono::system_clock::now();
-            }
+        if (timeDiff >= timeDelay) {
+            //std::cout << "Time: " << timeDiff << " DeltaX: " << deltaX << " DeltaY: " << deltaY << std::endl;
+
+            //reset counter
+            counter = std::chrono::system_clock::now();
+            processJSON(msSinceEpoch, deltaX, deltaY);
+            deltaX = 0;
+            deltaY = 0;
+        }
+
+        // Keycode 81 = Q
+        if (isKeyPressed(81)) {
+            std::cout << "Key with code " << 81 << " is pressed. Exiting..." << std::endl;
+            return 0; // Exit the program
+        }
 
 
     }
@@ -113,3 +193,4 @@ LRESULT CALLBACK EventHandler(
 
     return DefWindowProc(hwnd, event, wparam, lparam);
 }
+
